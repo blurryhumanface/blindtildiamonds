05a71e68aaa2727deaf2aad0011863ac66ca36d0
Adam Pettigrew <adam@pettigrew.org.uk>
2020-12-09 14:58:15 +0000

    Created the Command to blind the player that calls the command

--- BlindTilDiamonds/src/me/Sentaan/BlindTilDiamonds/Commands/BlindingCommand.java
+++ BlindTilDiamonds/src/me/Sentaan/BlindTilDiamonds/Commands/BlindingCommand.java
@@ -3,6 +3,9 @@ package me.Sentaan.BlindTilDiamonds.Commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import me.Sentaan.BlindTilDiamonds.Main;
 
@@ -10,13 +13,16 @@ public class BlindingCommand implements CommandExecutor{
 	private Main plugin;
 	
 	public BlindingCommand(Main plugin) {
-		
+		this.plugin = plugin;
+		plugin.getCommand("blind").setExecutor(this);
 	}
 	
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		// TODO Auto-generated method stub
+		Player p = (Player) sender;
+		
+		p.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 1000000, 255));
 		return false;
 	}
 
