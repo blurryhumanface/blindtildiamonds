29b49134da4fb17025b9d0fc1c64b7cd3d163627
Adam Pettigrew <adam@pettigrew.org.uk>
2020-12-09 15:00:14 +0000

    Called the Command classes and Listener classes and deleted extra class

--- BlindTilDiamonds/src/me/Sentaan/BlindTilDiamonds/Main.java
+++ BlindTilDiamonds/src/me/Sentaan/BlindTilDiamonds/Main.java
@@ -2,11 +2,16 @@ package me.Sentaan.BlindTilDiamonds;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import me.Sentaan.BlindTilDiamonds.Commands.BlindingCommand;
+import me.Sentaan.BlindTilDiamonds.Listeners.DiamondsListener;
+
 public class Main extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
 		System.out.println("(!) Blind 'Til Diamonds Online");
+		new BlindingCommand(this);
+		new DiamondsListener(this);
 	}
 	public void onDisable() {
 		System.out.println("(!) Blind 'Til Diamonds Offline");
